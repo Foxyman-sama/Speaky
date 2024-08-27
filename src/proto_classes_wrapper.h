deb8a9d5bfc4e3b0a721020ba32a0ff9c959a56f
Foxyman-sama <dimalatoho1448@gmail.com>
2024-08-27 17:21:06 +0300

    feat(deserialize): add functions to deserialize proto files

--- src/proto_classes_wrapper.h
+++ src/proto_classes_wrapper.h
@@ -9,15 +9,23 @@ namespace speaky {
 class ProtoClass {
  public:
   virtual std::string serialize() = 0;
+
+  virtual void deserialize(const std::string& serialized) = 0;
 };
 
 class MessageProto : public ProtoClass {
  public:
+  MessageProto() noexcept = default;
+
   MessageProto(const std::string& message) { obj.set_message(message); }
 
   MessageProto(const char* const message) { obj.set_message(message); }
 
   std::string serialize() override { return obj.SerializeAsString(); }
+
+  void deserialize(const std::string& serialized) override { obj.ParseFromString(serialized); }
+
+  std::string get_message() const noexcept { return obj.message(); }
 
  private:
   speaky::Message obj;
@@ -25,6 +33,8 @@ class MessageProto : public ProtoClass {
 
 class NewConnectionProto : public ProtoClass {
  public:
+  NewConnectionProto() noexcept = default;
+
   NewConnectionProto(const std::string& name, int room_id) {
     obj.set_name(name);
     obj.set_room_id(room_id);
@@ -36,6 +46,12 @@ class NewConnectionProto : public ProtoClass {
   }
 
   std::string serialize() override { return obj.SerializeAsString(); }
+
+  void deserialize(const std::string& serialized) override { obj.ParseFromString(serialized); }
+
+  std::string get_name() const noexcept { return obj.name(); }
+
+  int get_room_id() const noexcept { return obj.room_id(); }
 
  private:
   speaky::NewConnection obj;
--- tests/deserialize/deserialize_tests.cc
+++ tests/deserialize/deserialize_tests.cc
@@ -3,6 +3,9 @@
 
 #include <string>
 
+#include "proto/message.pb.h"
+#include "proto/new_connection.pb.h"
+#include "src/deserialize.h"
 #include "src/proto_classes_wrapper.h"
 
 using namespace testing;
@@ -15,3 +18,32 @@ int main() {
 }
 
 class deserialize_tests : public Test {};
+
+TEST_F(deserialize_tests, deserialize_message) {
+  constexpr const char* const test_message { "Hello, World!" };
+
+  speaky::Message message;
+  message.set_message(test_message);
+
+  const auto serialized { message.SerializeAsString() };
+
+  const auto actual { deserialize<MessageProto>(serialized) };
+
+  ASSERT_EQ(message.message(), actual.get_message());
+}
+
+TEST_F(deserialize_tests, deserialize_new_connection) {
+  constexpr const char* const test_name { "Igor" };
+  constexpr int test_room_id { 0 };
+
+  speaky::NewConnection new_conn;
+  new_conn.set_name(test_name);
+  new_conn.set_room_id(test_room_id);
+
+  const auto serialized { new_conn.SerializeAsString() };
+
+  const auto actual { deserialize<NewConnectionProto>(serialized) };
+
+  ASSERT_EQ(new_conn.name(), actual.get_name());
+  ASSERT_EQ(new_conn.room_id(), actual.get_room_id());
+}
