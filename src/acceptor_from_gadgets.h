cac0ef746a70731388f81930843a1e3c1883f80a
Foxyman-sama <dimalatoho1448@gmail.com>
2024-08-27 17:41:32 +0300

    add(acceptor_from_gadgets): add basic interface

---
+++ src/acceptor_from_gadgets.h
@@ -0,0 +1,12 @@
+#ifndef ACCEPTOR_FROM_GADGETS_H
+#define ACCEPTOR_FROM_GADGETS_H
+
+#include <boost/asio.hpp>
+
+namespace speaky {
+
+class AcceptorFromGadgets {};
+
+}  // namespace speaky
+
+#endif
