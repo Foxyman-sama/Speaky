aa7d52c0b7cf5a46b0fd1deecfd71ef28d68297a
Foxyman-sama <dimalatoho1448@gmail.com>
2024-08-29 14:43:33 +0300

    feat(unregister_user): add check on wrong id

--- src/input.h
+++ src/input.h
@@ -20,7 +20,10 @@ class Input {
     connect_participant_with_room(chat_id, participant);
   }
 
-  void unregister_user(int chat_id, ParticipantPtr participant) { rooms[chat_id]->kick(participant); }
+  void unregister_user(int chat_id, ParticipantPtr participant) {
+    check_room_existence(chat_id);
+    rooms[chat_id]->kick(participant);
+  }
 
  protected:
   bool is_room_found(int chat_id) const noexcept { return rooms.find(chat_id) != rooms.end(); }
@@ -30,6 +33,12 @@ class Input {
   void connect_participant_with_room(int chat_id, ParticipantPtr participant) {
     participant->attach(rooms[chat_id]);
     rooms[chat_id]->join(participant);
+  }
+
+  void check_room_existence(int chat_id) {
+    if (is_room_found(chat_id) == false) {
+      throw std::runtime_error { "The room wasn't found." };
+    }
   }
 
   std::unordered_map<int, ChatRoomPtr> rooms;
--- tests/unregister_user/unregister_user_tests.cc
+++ tests/unregister_user/unregister_user_tests.cc
@@ -19,15 +19,24 @@ int main() {
   return RUN_ALL_TESTS();
 }
 
-class unregister_user_tests : public Test, public Input {};
+class unregister_user_tests : public Test, public Input {
+ public:
+  void SetUp() {
+    participant = std::make_shared<ParticipantMock>("Dima");
+    register_user(chat_id, participant);
+
+    ASSERT_EQ(rooms[chat_id]->get_number_of_members(), 1);
+  }
+
+  static constexpr int chat_id { 0 };
+  std::shared_ptr<ParticipantMock> participant;
+};
 
 TEST_F(unregister_user_tests, unregister_common_user) {
-  constexpr int chat_id { 0 };
-  auto participant { std::make_shared<ParticipantMock>("Dima") };
-
-  register_user(chat_id, participant);
-  ASSERT_EQ(rooms[chat_id]->get_number_of_members(), 1);
-
   unregister_user(chat_id, participant);
   ASSERT_EQ(rooms[chat_id]->get_number_of_members(), 0);
+}
+
+TEST_F(unregister_user_tests, passes_wrong_chat_id_cause_exception) {
+  ASSERT_ANY_THROW(unregister_user(chat_id + 1, participant));
 }
