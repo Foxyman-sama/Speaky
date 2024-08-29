c70df47a74fea235fa7f9ab55a5c2e8e42f0f0d1
Foxyman-sama <dimalatoho1448@gmail.com>
2024-08-29 14:11:20 +0300

    test(unregister_user): add basic test interface

---
+++ tests/unregister_user/unregister_user_tests.cc
@@ -0,0 +1,19 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include "src/chat_room.h"
+#include "src/input.h"
+#include "src/participant.h"
+
+using namespace testing;
+using namespace speaky;
+
+int main() {
+  InitGoogleTest();
+  InitGoogleMock();
+  return RUN_ALL_TESTS();
+}
