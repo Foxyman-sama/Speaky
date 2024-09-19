bc5f13b098f66c340bdbe1bf3c89452894b4d15d
Foxyman-sama <dimalatoho1448@gmail.com>
2024-09-19 17:01:19 +0300

    fix(unregister_user): fix some modules after rebasing

--- src/input.h
+++ src/input.h
@@ -20,11 +20,6 @@ class Input {
     connect_participant_with_room(chat_id, participant);
   }
 
-  void unregister_user(int chat_id, ParticipantPtr participant) {
-    check_room_existence(chat_id);
-    kick(chat_id, participant);
-  }
-
  protected:
   bool is_room_found(int chat_id) const noexcept { return rooms.find(chat_id) != rooms.end(); }
 
@@ -34,14 +29,6 @@ class Input {
     participant->attach(rooms[chat_id]);
     rooms[chat_id]->join(participant);
   }
-
-  void check_room_existence(int chat_id) {
-    if (is_room_found(chat_id) == false) {
-      throw std::runtime_error { "The room wasn't found." };
-    }
-  }
-
-  void kick(int chat_id, ParticipantPtr participant) { rooms[chat_id]->kick(participant); }
 
   std::unordered_map<int, ChatRoomPtr> rooms;
 };
