0f1d41dc7282b773b53f1388bc705ce2712c63f2
Foxyman-sama <dimalatoho1448@gmail.com>
2024-09-30 16:06:36 +0300

    add(acceptor_from_gadgets): add connection functionality

--- src/acceptor_from_gadgets.h
+++ src/acceptor_from_gadgets.h
@@ -2,20 +2,14 @@
 #define ACCEPTOR_FROM_GADGETS_H
 
 #include <boost/asio.hpp>
-#include <boost/asio/error.hpp>
-#include <boost/asio/impl/read_until.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/address_v4.hpp>
-#include <boost/asio/registered_buffer.hpp>
-#include <boost/asio/streambuf.hpp>
-#include <boost/asio/write.hpp>
-#include <boost/system/detail/error_code.hpp>
 #include <exception>
 #include <future>
+#include <istream>
 #include <memory>
 #include <optional>
 #include <print>
 #include <stdexcept>
+#include <string>
 #include <thread>
 #include <utility>
 #include <variant>
@@ -35,6 +29,8 @@ class ParticipantFromGadgets : public Participant {
   explicit ParticipantFromGadgets(const std::string& name, boost::asio::ip::tcp::socket&& socket)
       : Participant { name }, socket { std::move(socket) } {}
 
+  void send(const std::string& message) {}
+
  private:
   boost::asio::ip::tcp::socket socket;
 };
@@ -53,6 +49,17 @@ class AcceptorFromGadgets {
 
   void accept() {
     for (;;) {
+      auto socket { acceptor.accept() };
+      boost::asio::streambuf buf;
+      boost::asio::read_until(socket, buf, delim);
+
+      std::string temp;
+      std::istream is { &buf };
+      std::getline(is, temp, delim);
+
+      auto new_connection { deserialize<NewConnectionProto>(temp) };
+      auto participant { std::make_shared<ParticipantFromGadgets>(new_connection.get_name(), std::move(socket)) };
+      input.register_user(new_connection.get_room_id(), participant);
     }
   }
 
