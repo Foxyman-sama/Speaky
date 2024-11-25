63023244f460a21730aa48c3b02907c501a422e8
Foxyman-sama <dimalatoho1448@gmail.com>
2024-11-25 15:59:39 +0200

    feat: add convert from json to registration package

--- tests/package/package_tests.cc
+++ tests/package/package_tests.cc
@@ -17,11 +17,21 @@ int main() {
 }
 
 struct RegisterPackage {
+  RegisterPackage(const std::string& room_id, const std::string& username)
+      : room_id { room_id }, username { username } {}
+
+  RegisterPackage(const boost::property_tree::ptree& ptree)
+      : room_id { ptree.get<std::string>("room_id") }, username { ptree.get<std::string>("username") } {}
+
   boost::property_tree::ptree make_json() {
     boost::property_tree::ptree result;
     result.put("room_id", room_id);
     result.put("username", username);
     return result;
+  }
+
+  constexpr bool operator==(const RegisterPackage& other) const noexcept {
+    return room_id == other.room_id && username == other.username;
   }
 
   std::string room_id;
@@ -36,6 +46,18 @@ TEST(package_tests, convert_to_json) {
   RegisterPackage package { "1", "Dima" };
 
   const auto actual { package.make_json() };
+
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(package_tests, convert_to_struct_from_json) {
+  const RegisterPackage expected { "1", "Dima" };
+
+  boost::property_tree::ptree ptree;
+  ptree.put("room_id", "1");
+  ptree.put("username", "Dima");
+
+  const RegisterPackage actual { ptree };
 
   ASSERT_EQ(expected, actual);
 }
