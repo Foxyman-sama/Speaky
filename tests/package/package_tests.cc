f0729b02f38e4c9746f536151df69c4df534ecb2
Foxyman-sama <dimalatoho1448@gmail.com>
2024-11-25 15:52:59 +0200

    feat: add registration package and convertation to json

--- tests/package/package_tests.cc
+++ tests/package/package_tests.cc
@@ -1,6 +1,10 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ptree_fwd.hpp>
+
 #include "boost/asio/io_context.hpp"
 #include "src/chat.hpp"
 
@@ -10,4 +14,28 @@ int main() {
   InitGoogleTest();
   InitGoogleMock();
   return RUN_ALL_TESTS();
+}
+
+struct RegisterPackage {
+  boost::property_tree::ptree make_json() {
+    boost::property_tree::ptree result;
+    result.put("room_id", room_id);
+    result.put("username", username);
+    return result;
+  }
+
+  std::string room_id;
+  std::string username;
+};
+
+TEST(package_tests, convert_to_json) {
+  boost::property_tree::ptree expected;
+  expected.put("room_id", "1");
+  expected.put("username", "Dima");
+
+  RegisterPackage package { "1", "Dima" };
+
+  const auto actual { package.make_json() };
+
+  ASSERT_EQ(expected, actual);
 }
